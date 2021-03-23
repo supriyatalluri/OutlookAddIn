489942ee4f1c697fc0ef7a40ce56befcd43f313e
Talluri Angel Supriya <taangels@microsoft.com>
2021-03-23 19:07:32 +0530

    new changes

--- src/addin/taskpane.js
+++ src/addin/taskpane.js
@@ -8,15 +8,20 @@ import "../../assets/icon-16.png";
 import "../../assets/icon-32.png";
 import "../../assets/icon-80.png";
 
-'use strict';
+/* global document, Office */
 
 Office.onReady(info => {
   if (info.host === Office.HostType.Outlook) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
   }
 });
 
 export async function run() {
-  var item = Office.context.mailbox.item;
-  document.getElementById("recurrence").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
+  // Get a reference to the current message
+var item = Office.context.mailbox.item;
+
+// Write message property value to the task pane
+document.getElementById("item-subject").innerHTML = "<b>Subject:</b> <br/>" + item.subject;
 }
